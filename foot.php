0cc4aa0932576491a4b1ba8ce8d9663d5e90bbf3
HerMan <infabls.com@gmail.com>
2020-04-28 12:38:49 +0600

    add bitrix chat

--- foot.php
+++ foot.php
@@ -136,9 +136,11 @@
 
 <script src="assets/css/popup/poup.js"></script>
 <script src="assets/css/popup/popup.css"></script>
+<!-- bitrix chat -->
 <script>
         (function(w,d,u){
                 var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
                 var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
         })(window,document,'https://cdn-ru.bitrix24.kz/b3194113/crm/site_button/loader_6_ulfhx4.js');
 </script>
+<!-- bitrix chat -->
