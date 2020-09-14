5a8a43c79b11ff334df2b835bd65a26a799b787f
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-09-14 14:12:16 +0600

    Update showcase.php

--- showcase.php
+++ showcase.php
@@ -23,65 +23,239 @@
       <div class="grid-block__content container-fluid container__showcase">
         <div class="row">
           <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
-            <a class="grid-block__item grid-block__item_active" href="#1.php">
+            <a class="grid-block__item grid-block__item_active" href="pages/showcase/showcase1.php">
               <!-- <span class="grid-block__year">001</span> -->
-              <span class="grid-block__item-content"><span class="grid-block__subtitle">АКБ</span><span class="grid-block__title">Cвинцово-гелевые</span></span>
+              <span class="grid-block__item-content"><span class="grid-block__subtitle">АКБ</span><span class="grid-block__title">ПЭТ белый ,синий. (прессованый)</span></span>
             </a>
           </div>
           <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
             <a class="grid-block__item grid-block__item_active" href="#2.php">
               <!-- <span class="grid-block__year">002</span> -->
-              <span class="grid-block__item-content"><span class="grid-block__subtitle">АКБ </span><span class="grid-block__title">Cвинцово-кислотные</span></span>
+              <span class="grid-block__item-content"><span class="grid-block__subtitle">АКБ </span><span class="grid-block__title">ПЭТ коричневый, зеленый. (прессованый)</span></span>
             </a>
           </div>
           <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
             <a class="grid-block__item grid-block__item_active" href="#3.php">
               <!-- <span class="grid-block__year">003</span> -->
-              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Картон</span></span>
+              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Канистры ПНД.(белая)</span></span>
             </a>
           </div>
           <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
             <a class="grid-block__item grid-block__item_active" href="#4.php">
              <!--  <span class="grid-block__year">004</span> -->
-              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Макулатура</span></span>
+              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Канистры ПНД .(цветная)</span></span>
             </a>
           </div>
           <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
             <a class="grid-block__item grid-block__item_active" href="#5.php">
              <!--  <span class="grid-block__year">004</span> -->
-              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Масло отработанное</span></span>
+              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластиковые ящики вино водка</span></span>
             </a>
-          </div>   
-                <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+          </div>   ы
+          <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
             <a class="grid-block__item grid-block__item_active" href="#6.php">
              <!--  <span class="grid-block__year">004</span> -->
-              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">ПНД канистры</span></span>
-            </a>
-          </div>
-          <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
-            <a class="grid-block__item grid-block__item_active" href="#7.php">
-             <!--  <span class="grid-block__year">004</span> -->
-              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Полиэтиленовая пленка</span></span>
-            </a>
-          </div>
-                    <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
-            <a class="grid-block__item grid-block__item_active" href="#8.php">
-             <!--  <span class="grid-block__year">004</span> -->
-              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пэт-тара</span></span>
-            </a>
-          </div>
-          <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
-            <a class="grid-block__item grid-block__item_active" href="#9.php">
-             <!--  <span class="grid-block__year">004</span> -->
-              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Стрейч-пленка</span></span>
-            </a>
-          </div>
-          <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
-            <a class="grid-block__item grid-block__item_active" href="#10.php">
-             <!--  <span class="grid-block__year">004</span> -->
-              <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Фритюрное масло</span></span>
-            </a>
-          </div>
+             <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластиковые ящики из-под овощей</span></span>
+           </a>
+         </div>
+         <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+          <a class="grid-block__item grid-block__item_active" href="#7.php">
+           <!--  <span class="grid-block__year">004</span> -->
+           <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Трубы черные ПНД с синей полосой</span></span>
+         </a>
+       </div>
+       <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+        <a class="grid-block__item grid-block__item_active" href="#8.php">
+         <!--  <span class="grid-block__year">004</span> -->
+         <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Полиэтиленовая пленка ПВД прозрачная (прессованая)</span></span>
+       </a>
+     </div>
+     <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+      <a class="grid-block__item grid-block__item_active" href="#9.php">
+       <!--  <span class="grid-block__year">004</span> -->
+       <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Полиэтиленовая пленка ПВД цветная (прессованая)</span></span>
+     </a>
+   </div>
+   <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+    <a class="grid-block__item grid-block__item_active" href="#10.php">
+     <!--  <span class="grid-block__year">004</span> -->
+     <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Стрейч-пленка прессованая</span></span>
+   </a>
+ </div>
+ <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Профиль оконный. ПВХ белый</span></span>
+ </a>
+</div>         
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>           <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>           <div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
+<div class="grid-block__column col-12 col-sm-6 col-lg-4 col-xl-3">
+  <a class="grid-block__item grid-block__item_active" href="#10.php">
+   <!--  <span class="grid-block__year">004</span> -->
+   <span class="grid-block__item-content"><!-- <span class="grid-block__subtitle">Продукт</span> --><span class="grid-block__title">Пластик дробленный от орг.техники ABS</span></span>
+ </a>
+</div>
         </div>
       </div>
     </div>
