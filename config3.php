b8fd98d56debb44c1dfa4c47ebc5d2bdd4293cad
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-09-17 10:35:01 +0600

    config upd

---
+++ config3.php
@@ -0,0 +1,9 @@
+<?php 
+$dev_mode = false;
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "kzsr";
+$base_uri = "https://kzsr/";
+$db_uri = "https://kzsr/";
+$url = $db_uri . $_SERVER['REQUEST_URI'];
