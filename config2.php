08640b473663c323889409d30136c0156803acf9
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-09-19 00:06:53 +0600

    cnfg upd

--- config2.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$dev_mode = false;
-$servername = "srv-pleskdb33.ps.kz:3306";
-$username = "kzsrkz_root";
-$password = "Root!,@,#,$,%,^,&,*,?,_,~";
-$dbname = "kzsrkz_kzsrkz";
-$base_uri = "https://kzsr.kz";
-$db_uri = "https://kzsr.kz";
-$url = $db_uri . $_SERVER['REQUEST_URI'];
